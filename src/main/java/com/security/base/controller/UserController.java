2c5a1c864ffca432941d9b9b22fa8cace83dea51
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-01-23 11:30:25 -0300

    terminando a parte básica da authenticação

--- src/main/java/com/security/base/controller/UserController.java
+++ src/main/java/com/security/base/controller/UserController.java
@@ -1,2 +1,22 @@
-package com.security.base.controller;public class user {
+package com.security.base.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/public")
+    String publicAccess() {
+        return "<h1>acesso aprovado!</h1>";
+    }
+
+    @GetMapping("/private")
+    String privateAccess() {
+        return "<h1>acesso privado aprovado!</h1>";
+    }
+
+
 }
--- src/main/java/com/security/base/security/SecurityConfig.java
+++ src/main/java/com/security/base/security/SecurityConfig.java
@@ -1,2 +1,27 @@
-package com.security.base.security;public class SecurityConfig {
+package com.security.base.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(
+                        authorizeConfig -> {
+                            authorizeConfig.requestMatchers("/user/public").permitAll();
+                            authorizeConfig.requestMatchers("/logout").permitAll();
+                            authorizeConfig.anyRequest().authenticated();
+                        }
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
 }
