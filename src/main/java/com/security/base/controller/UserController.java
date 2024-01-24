6254da4dbfe71d2a27f286ee7fb707b1bc49072a
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-01-24 10:55:50 -0300

    finishing

--- src/main/java/com/security/base/controller/UserController.java
+++ src/main/java/com/security/base/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.security.base.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,14 @@ public class UserController {
     }
 
     @GetMapping("/private")
-    String privateAccess() {
-        return "<h1>acesso privado aprovado!</h1>";
+    String privateAccess(@AuthenticationPrincipal OidcUser principal) { //OIDC = open id connected
+        return String.format("""
+                <h1>Rota privada acessada</h1> <br><br>
+                <h3>Principal: %s</h3> <br><br>
+                <h3>Email attribute: %s </h3> <br><br>
+                <h3>Authorities: %s </h3> <br><br>
+                <h3>JWT: %s </h3> <br><br>
+                """, principal, principal.getAttribute("email"), principal.getAuthorities(), principal.getIdToken().getTokenValue());
     }
 
 
